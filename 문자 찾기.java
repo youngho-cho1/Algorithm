3031d2d2f4a89b9c212025f8e3769789c86d4421
youngho <mmy4637@naver.com>
2023-01-17 18:49:10 +0900

    Create 문자 찾기.java

---
+++ 문자 찾기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main{
+  public int solution(String str, char t){
+    int answer = 0;
+    str = str.toUpperCase();
+    t = Character.toUpperCase(t);
+    for(int i=0; i<str.length(); i++){
+      if(str.charAt(i)==t) answer++;
+    }
+    return answer;
+  }
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+    char c = kb.next().charAt(0);
+    System.out.print(T.solution(str,c));
+  }
+}
