c59ab82cdff5ecad3efbd422bfb667e924675ad1
youngho <mmy4637@naver.com>
2023-01-25 13:08:37 +0900

    Create 단어 뒤집기.java

---
+++ 단어 뒤집기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main {
+	public ArrayList<String> solution(int n, String[] str){
+      ArrayList<String> answer = new ArrayList<>();
+      for(String x : str){
+        String tmp = new StringBuilder(x).reverse().toString();
+        answer.add(tmp);
+      }
+      return answer;
+      }
+      public static void main(String[] args){
+      	Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for(int i=0; i<n; i++){
+          str[i] = kb.next();
+        }
+        for(String x: T.solution(n, str)){
+        	System.out.println(x);
+        }
+      }
+}
