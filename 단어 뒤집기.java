bb0d96cd2e6b79caf88f8139640721057f9ddccf
youngho <mmy4637@naver.com>
2023-01-26 18:25:46 +0900

    Update 단어 뒤집기.java

--- 단어 뒤집기.java
+++ 단어 뒤집기.java
@@ -5,8 +5,10 @@ class Main {
       for(String x : str){
         String tmp = new StringBuilder(x).reverse().toString();
         answer.add(tmp);
+	      
       }
       return answer;
+		
       }
       public static void main(String[] args){
       	Main T = new Main();
