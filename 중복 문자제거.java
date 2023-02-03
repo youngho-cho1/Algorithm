720f0eea1a80927e6572a3a7df5fcc0296fa006c
youngho <mmy4637@naver.com>
2023-02-03 20:34:56 +0900

    Create 중복 문자제거.java

---
+++ 중복 문자제거.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Main {
+	public String solution(String str){
+      String answer = "";
+      for(int i=0; i<str.length(); i++){
+        	if(str.indexOf(str.charAt(i))==i) answer += str.charAt(i);
+      }
+      return answer;
+    }
+  public static void main(String[] args){
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    String str = kb.next();
+    System.out.print(T.solution(str));
+  }
+}
